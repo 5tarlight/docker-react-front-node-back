29cdffba34137b56daca9f984a69b4334de69cdb
5tarlight <developerstarlight@gmail.com>
2021-05-03 09:26:53 +0900

    docker completion

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 
 app.set('port', process.env.PORT | 5676)
 
+app.route('/').get((req, res) => {
+  console.log(`${req.ip}`)
+  res.end('Hello World')
+})
+
 app.listen(app.get('port'), () => {
   console.log('Server is ready')
 })
